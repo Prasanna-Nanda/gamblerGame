5755fd552a76b09c7870fbc5350dbbca27fa7826
Prasanna-Nanda <prasannanandakumar92@gmail.com>
2021-05-03 20:13:56 +0530

    Added Player_wonOrLose

--- gamblernew.js
+++ gamblernew.js
@@ -5,11 +5,18 @@ let wins = 0;
 let lose = 0;
 const bets = 1;
 
+ function getGambler(){
+let random = Math.floor(Math.random() * 10) % 2;
 if (random == 1){
   wins ++;
-console.log(stake);
+//console.log(stake);
+ stake = stake + bets;
 }else{
   lose ++;
-console.log(stake);
+//console.log(stake);
+ stake = stake + bets;
 }
 return stake;
+}
+
+getGambler();
