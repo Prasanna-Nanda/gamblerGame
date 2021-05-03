c0b3ee0f09a2332eaa54ebf7f7b88d789f69b4c1
Prasanna-Nanda <prasannanandakumar92@gmail.com>
2021-05-03 20:25:39 +0530

    Added StakeRemain

--- gamblernew.js
+++ gamblernew.js
@@ -5,18 +5,35 @@ let wins = 0;
 let lose = 0;
 const bets = 1;
 
- function getGambler(){
-let random = Math.floor(Math.random() * 10) % 2;
-if (random == 1){
-  wins ++;
-//console.log(stake);
- stake = stake + bets;
-}else{
-  lose ++;
-//console.log(stake);
- stake = stake + bets;
-}
-return stake;
-}
+function getGambler() {
+  let wins = 0;
+  let lose = 0;
+  stake = 100;
+  while(stake > 50 && stake < 150){
 
-getGambler();
+    let random = Math.floor(Math.random() * 10) % 2
+    if (random == 0){
+      wins ++;
+
+    // console.log("player wins ")
+
+    stake = stake + bets
+  // console.log(stake)
+     }else{
+       lose ++;
+    // console.log(" player lose 1$ ")
+
+    stake = stake - bets
+    // console.log(stake)
+      }
+    }
+          return stake;
+    }
+
+function totalAmount() {
+if (totalAmount == 150) {
+} else {
+amount_lose = amount_lose + totalAmount
+}
+ gettotalAmount();
+
