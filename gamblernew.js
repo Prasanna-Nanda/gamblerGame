e1d592c85feb4264afa2decd0abd56ac0d9be42d
Prasanna-Nanda <prasannanandakumar92@gmail.com>
2021-05-03 20:05:03 +0530

    Added gambler_start-game

---
+++ gamblernew.js
@@ -0,0 +1,15 @@
+console.log("welcome to Gambler Game")
+
+let stake = 100;
+let wins = 0;
+let lose = 0;
+const bets = 1;
+
+if (random == 1){
+  wins ++;
+console.log(stake);
+}else{
+  lose ++;
+console.log(stake);
+}
+return stake;
